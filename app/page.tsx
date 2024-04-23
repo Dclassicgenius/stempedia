01b784f1687750fcd64644df8d49828ff52ca88e
Dclassicgenius <patamaechi1@gmail.com>
2024-04-23 23:38:35 +0300

    added link to game

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import Explore from "@/components/Explore";
 import Hero from "@/components/Hero";
-import NavBar from "@/components/NavBar";
 import News from "@/components/News";
 import Services from "@/components/Services";
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="min-h-screen space-y-7">
+    <main className="min-h-screen space-y-14">
       <Hero />
       <Explore />
       <News />
--- components/Services.tsx
+++ components/Services.tsx
@@ -2,14 +2,24 @@ import Image from "next/image";
 import Link from "next/link";
 
 const services = [
-  { title: "Videos", description: "", image: "/stem-1.jpeg" },
-  { title: "Courses", description: "", image: "/online-course.svg" },
-  { title: "Infographics", description: "", image: "/stem-4.jpeg" },
-  { title: "Simulations", description: "", image: "/stem-2.png" },
-  { title: "Games", description: "", image: "/video-game.svg" },
-  { title: "Written Guides", description: "", image: "/stem-3.jpeg" },
-  { title: "Practice", description: "", image: "/practice.jpeg" },
-  { title: "Blog", description: "", image: "/blog.jpeg" },
+  { title: "Videos", description: "", image: "/stem-1.jpeg", link: "#" },
+  { title: "Courses", description: "", image: "/online-course.svg", link: "#" },
+  { title: "Infographics", description: "", image: "/stem-4.jpeg", link: "#" },
+  { title: "Simulations", description: "", image: "/stem-2.png", link: "#" },
+  {
+    title: "Games",
+    description: "",
+    image: "/video-game.svg",
+    link: "https://www.figma.com/proto/u6ZLhc0YDAgaRsAU2F3gRH/IAEA?page-id=0%3A1&type=design&node-id=3-3&viewport=506%2C-782%2C0.32&t=yeJaFrmtC7HKcCun-1&scaling=min-zoom&mode=design",
+  },
+  {
+    title: "Written Guides",
+    description: "",
+    image: "/stem-3.jpeg",
+    link: "#",
+  },
+  { title: "Practice", description: "", image: "/practice.jpeg", link: "#" },
+  { title: "Blog", description: "", image: "/blog.jpeg", link: "#" },
 ];
 const Services = () => {
   return (
@@ -22,6 +32,7 @@ const Services = () => {
             title={service.title}
             description={service.description}
             image={service.image}
+            link={service.link}
           />
         ))}
       </div>
@@ -35,24 +46,24 @@ type ServiceCardProps = {
   title: string;
   description: string;
   image: string;
+  link: string;
 };
 
-const ServiceCard = ({ title, description, image }: ServiceCardProps) => {
+const ServiceCard = ({ title, description, image, link }: ServiceCardProps) => {
   return (
     <div className="flex gap-4 hover:scale-110 transition-all flex-col items-center justify-center">
-      <Image
-        src={image}
-        alt={title}
-        width={120}
-        height={100}
-        className="object-contain h-full"
-      />
-      <div className="flex flex-col justify-center gap-4">
-        <h2 className="text-2xl font-bold">{title}</h2>
-        <Link href="#" className=" text-sm font-medium">
-          {description}
-        </Link>
-      </div>
+      <Link href={link} target="_blank">
+        <Image
+          src={image}
+          alt={title}
+          width={120}
+          height={100}
+          className="object-contain h-full"
+        />
+        <div className="flex flex-col justify-center gap-4">
+          <h2 className="text-2xl font-bold">{title}</h2>
+        </div>
+      </Link>
     </div>
   );
 };
